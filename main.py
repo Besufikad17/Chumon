211611438297cf30a2cb6cce26bfb9415bab39d6
Besufikad17 <besufikadtilahun1994@gmail.com>
2022-10-24 13:36:22 +0300

    error handling and inline button added

--- main.py
+++ main.py
@@ -4,40 +4,45 @@ import requests
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 load_dotenv()
 
 
 API_TOKEN = os.getenv('API_TOKEN')
 PROXY_URL = os.getenv('PROXY_URL')
-PROXY_AUTH = aiohttp.BasicAuth(login='login', password=os.getenv('PROXY_PWD'))
 
 logging.basicConfig(level=logging.INFO)
 
-# bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
 def get_order(title: str):
     req = requests.get(f'http://localhost:4000/api/order/{title}')
-    print(req.json())
     return req.json()
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm ChūmonBot!\n To get order of a specific anime enter /order <ANIME TITLE>")
+    await message.reply("Hi!\nI'm ChūmonBot!\n To get order of a specific anime enter /order <ANIME-TITLE>")
 
 
 @dp.message_handler(commands=['order'])
 async def send_watch_order(message: types.Message):
-    anime_title = message.text.split(' ')[1]
+    anime_title = message.text[7:]
+    print(anime_title)
     if anime_title is None:
         await message.reply("That's not quite right!! \n To get order of a specific anime enter /order <ANIME TITLE>")
     else:
-        get_order(anime_title)
-        await message.reply(get_order(anime_title)[0]['order'], parse_mode = "Markdown")
+        res = get_order(anime_title)
+        
+        if res is None or len(res) == 0:
+            await message.reply("Can't find the anime :( try alternative titles")
+        else:
+            rkm = InlineKeyboardMarkup(row_wigth=1)
+            rkm.add(InlineKeyboardButton("More..", "https://www.reddit.com/r/anime/wiki/watch_order/"))
+            await message.answer(res[0]['order'], reply_markup=rkm)
 
 
 @dp.message_handler()
